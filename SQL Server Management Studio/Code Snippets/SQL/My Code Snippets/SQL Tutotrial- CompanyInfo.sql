52912883ba8d3dc3363cb0c5dec2b7cb127ea8c3
Divine Jude <judedivine68@gmail.com>
2023-02-25 00:20:40 +0100

    Update SQL Tutotrial- CompanyInfo.sql

--- SQL Server Management Studio/Code Snippets/SQL/My Code Snippets/SQL Tutotrial- CompanyInfo.sql
+++ SQL Server Management Studio/Code Snippets/SQL/My Code Snippets/SQL Tutotrial- CompanyInfo.sql
@@ -1,64 +1,59 @@
 -- Creating a Company Database
--- CREATE DATABASE CompanyDatabase;
+CREATE DATABASE CompanyDatabase;
 
 -- Creating Tables and schema
--- CREATE TABLE Employee (
---     emp_id INT PRIMARY KEY,
---     first_name VARCHAR(40),
---     last_name VARCHAR(40),
---     birth_date DATE,
---     sex VARCHAR(1),
---     salary INT,
---     super_id INT,
---     branch_id INT,
---     FOREIGN KEY(super_id) REFERENCES Employee(emp_id) ON DELETE NO ACTION
--- );
+CREATE TABLE Employee (
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    birth_date DATE,
+    sex VARCHAR(1),
+    salary INT,
+    super_id INT,
+    branch_id INT,
+    FOREIGN KEY(super_id) REFERENCES Employee(emp_id) ON DELETE NO ACTION
+);
 
 
--- CREATE TABLE Branch(
---     branch_id INT PRIMARY KEY,
---     branch_name VARCHAR(40),
---     mgr_id INT,
---     mgr_start_date DATE,
---     FOREIGN KEY(mgr_id) REFERENCES Employee(emp_id) ON DELETE SET NULL
--- );
+CREATE TABLE Branch(
+    branch_id INT PRIMARY KEY,
+    branch_name VARCHAR(40),
+    mgr_id INT,
+    mgr_start_date DATE,
+    FOREIGN KEY(mgr_id) REFERENCES Employee(emp_id) ON DELETE SET NULL
+);
 
--- ALTER TABLE Employee 
--- ADD FOREIGN KEY(branch_id)
--- REFERENCES Branch(branch_id) ON DELETE SET NULL;
+ALTER TABLE Employee 
+ADD FOREIGN KEY(branch_id)
+REFERENCES Branch(branch_id) ON DELETE SET NULL;
 
--- CREATE TABLE Branch_Supplier (
---     branch_id INT,
---     supplier_name VARCHAR(40),
---     supply_type VARCHAR(40),
---     PRIMARY KEY(branch_id, supplier_name),
---     FOREIGN KEY(branch_id) REFERENCES Branch(branch_id) ON DELETE CASCADE
--- );
+CREATE TABLE Branch_Supplier (
+    branch_id INT,
+    supplier_name VARCHAR(40),
+    supply_type VARCHAR(40),
+    PRIMARY KEY(branch_id, supplier_name),
+    FOREIGN KEY(branch_id) REFERENCES Branch(branch_id) ON DELETE CASCADE
+);
 
--- CREATE TABLE client (
---     client_id INT PRIMARY KEY,
---     client_name VARCHAR(40),
---     branch_id INT,
---     FOREIGN KEY (branch_id) REFERENCES Branch(branch_id) ON DELETE SET NULL
--- );
+CREATE TABLE client (
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(40),
+    branch_id INT,
+    FOREIGN KEY (branch_id) REFERENCES Branch(branch_id) ON DELETE SET NULL
+);
 
--- CREATE TABLE works_with (
---     emp_id INT,
---     client_id INT,
---     total_sales INT,
---     PRIMARY KEY(emp_id, client_id),
---     FOREIGN KEY(emp_id) REFERENCES Employee(emp_id) ON DELETE CASCADE,
---     FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
--- );
+CREATE TABLE works_with (
+    emp_id INT,
+    client_id INT,
+    total_sales INT,
+    PRIMARY KEY(emp_id, client_id),
+    FOREIGN KEY(emp_id) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+    FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+);
 
 
 -- -----------------------------------------------------------------------------
 
--- DELETE FROM Employee;
--- DELETE FROM Branch;
--- DELETE FROM Branch_Supplier;
--- DELETE FROM client;
--- DELETE FROM works_with;
 
 -- Corporate
 INSERT INTO Employee VALUES(100, 'David', 'Wallace', '1967-11-17', 'M', 250000, NULL, NULL);
